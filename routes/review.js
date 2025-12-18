000b84f9d1b5fd0d81fd867b9344575aae318f2d
meghraj <khanatmeghraj@gmail.com>
2025-12-18 14:48:00 +0530

    added router folder

--- routes/review.js
+++ routes/review.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({mergeParams: true}); 
 const wrapAsync = require("../utils/wrapAsync");
 const ExpressError = require("../utils/ExpressError");
 const { reviewSchema } = require("../schema");
