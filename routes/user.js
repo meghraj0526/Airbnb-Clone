917f795ab2324c8268c77a6b2a312f2c1ccff077
meghraj <khanatmeghraj@gmail.com>
2025-12-20 13:22:59 +0530

    added login/signup page

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,46 @@
-const express = require('express');
-const router = express.Router(); 
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
-router.get('/signup', (req, res) => {
-    res.render("users/signup.ejs");
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
 });
+
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      // Here you would typically add code to save the user to your database
+      const newUser = new User({ username, email });
+      const registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.flash("success", "Welcome to the site!");
+      res.redirect("/listings");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
+    }
+  })
+);
+
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  wrapAsync(async (req, res) => {
+    req.flash("success", "Welcome back!");
+    res.redirect("/listings");
+  })
+);
 
 module.exports = router;
