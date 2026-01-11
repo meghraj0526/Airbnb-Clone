022469600f9660308f78d59683386144e2656663
meghraj <khanatmeghraj@gmail.com>
2026-01-11 15:18:37 +0530

    Search compoentent added

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initdata = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGOO_URL = "mongodb://127.0.0.1:27017/WanderlustDB";
+const MONGOO_URL = "mongodb+srv://meghrajkhanat:%23mekhanat0525@cluster0.jvxw6rh.mongodb.net/WanderlustDB?appName=Cluster0";
 
 main()
   .then(() => console.log("Database connected"))
@@ -16,7 +16,7 @@ const initDB = async () => {
   await Listing.deleteMany({});
   initdata.data = initdata.data.map((obj) => ({
     ...obj,
-    owner: "6945476fbcff5aff0711a421",
+    owner: "69636afc437151ccaaaf6113",
   }));
   await Listing.insertMany(initdata.data);
   console.log("Database initialized");
